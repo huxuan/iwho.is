a7cd3c5ef8477df7a226826d129a67699b24867f
huxuan <i@huxuan.org>
2014-11-06 00:56:54 +0800

    Cache should work now. Fix #3.
    - Use redis as LRU cache.
    - Use Queue and threading for background function call.
    - Bug fix for using pass in try except.

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -6,6 +6,11 @@ Author: huxuan
 Email: i(at)huxuan.org
 Description: Initialization for app.
 """
+import Queue
+import threading
+import time
+
+import redis
 
 from flask import Flask
 from flask.ext.babel import Babel
@@ -15,5 +20,23 @@ app = Flask(__name__)
 app.config.from_object('config')
 babel = Babel(app)
 Bootstrap(app)
+
+tasks = Queue.Queue()
+
+def tasks_worker():
+    while True:
+        if tasks.empty():
+            time.sleep(5)
+        else:
+            tasks.get().start()
+            time.sleep(1)
+
+thread = threading.Thread(target=tasks_worker)
+thread.daemon = True
+thread.start()
+
+db = redis.StrictRedis()
+db.config_set('maxmemory', '100000000')
+db.config_set('maxmemory-policy', 'allkeys-lru')
 
 from app import views
--- src/app/lib.py
+++ src/app/lib.py
@@ -8,12 +8,18 @@ Description: library used in app.
 """
 import re
 import socket
+import threading
 
 import ipaddr
 import ipwhois
 import pythonwhois
 
+from app import db
+from app import tasks
+
 DOMAIN_PATTERN = re.compile('^([a-z0-9]+(-[a-z0-9]+)*\.)+[a-z]{2,}$')
+REDIS_KEY_IP = 'iwhois_ip_%s'
+REDIS_KEY_DOMAIN = 'iwhois_domain_%s'
 
 def get_whois(query):
     """docstring for get_whois"""
@@ -36,20 +42,41 @@ def get_whois(query):
             'res': res,
         }
     except:
-        pass
+        raise
 
 def get_whois_ip(ip):
     """docstring for get_whois_ip"""
+    res = db.get(REDIS_KEY_IP % ip)
+    if res:
+        return res
+    else:
+        return get_real_whois_ip(ip)
+
+def get_real_whois_ip(ip):
+    """docstring for get_real_whois_ip"""
     ip = ipwhois.IPWhois(ip)
-    res = ip.lookup(inc_raw=True)['raw']
-    return convert_newline(res)
+    res = convert_newline(ip.lookup(inc_raw=True)['raw'])
+    db.set(REDIS_KEY_IP % ip, res)
+    return res
 
 def get_whois_domain(domain):
     """docstring for get_whois_domain"""
+    res = db.get(REDIS_KEY_DOMAIN % domain)
+    if res:
+        thread = threading.Thread(target=get_real_whois_domain, args=[domain])
+        thread.daemon = True
+        tasks.put(thread)
+        return res
+    else:
+        return get_real_whois_domain(domain)
+
+def get_real_whois_domain(domain):
+    """docstring for get_real_whois_domain"""
     res = pythonwhois.net.get_whois_raw(domain)
     res.reverse()
-    res = '\n'.join(res)
-    return convert_newline(res)
+    res = convert_newline('\n'.join(res))
+    db.set(REDIS_KEY_DOMAIN % domain, res)
+    return res
 
 def convert_newline(s):
     s = s.replace('\r\n', '<br/>')
